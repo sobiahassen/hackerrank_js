4986a52bbb7cce5aa6ed2e7144904df413662a6a
Sobia Adil <sobia.hassen@gmail.com>
2017-08-18 00:08:21 +0530

    Add min max

---
+++ min_max.js
@@ -0,0 +1,37 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+function adder(arr) {
+    var sum = 0;
+    for (count = 0; count < 4; count++) {
+         sum += arr[count];
+     }
+    return sum;
+}
+
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+   var arr = readLine().split(' ');
+       arr = arr.map(Number);
+       arr = arr.sort();
+       console.log(adder(arr).toString() +" " + adder(arr.reverse()).toString());
+}
